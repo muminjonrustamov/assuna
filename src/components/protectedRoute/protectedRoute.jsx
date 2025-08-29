753535dfa8e302aa768d1a21fa4e251e13505192
Muminjon <119095876+muminjonrustamov@users.noreply.github.com>
2025-08-29 11:26:51 +0500

    password

--- src/components/protectedRoute/protectedRoute.jsx
+++ src/components/protectedRoute/protectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 import SHA256 from 'crypto-js/sha256';
 
 const ProtectedRoute = ({ children }) => {
-  const expectedToken = SHA256('admin12345').toString();
+  const expectedToken = SHA256('adminassuna01').toString();
   const token = localStorage.getItem('token');
 
   return token === expectedToken ? children : <Navigate to="/login" replace />;
